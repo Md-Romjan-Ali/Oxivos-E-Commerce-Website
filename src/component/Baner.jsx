14ba72e0b60ec34cefeb10f5c98d2b26f8ed96e0
Md-Romjan-Ali <mdromjanali224512@gmail.com>
2026-07-16 08:17:17 +0600

    Create Baner section

--- src/component/Baner.jsx
+++ src/component/Baner.jsx
@@ -1,14 +1,28 @@
-import React, { useEffect } from 'react';
+'use clinet'
+import React, { useEffect, useState } from 'react';
+
 
 const Baner = () => {
+    const [data, setData] = useState([])
     useEffect(() => {
         fetch(`https://oxivos-e-commerce.vercel.app/baner.json`)
+            .then(req => req.json())
+            .then(res => setData(res))
     }, []
     )
     return (
-        <div className="bg-[url('https://img.magnific.com/premium-vector/online-shopping-banner-ecommerce-sale-ad-digital-store-promotion-online-shopping-offer_1377942-481.jpg?semt=ais_hybrid&w=740&q=80')] bg-cover bg-no-repeat bg-center h-[60vh] w-full">
+        <div className="bg-[url('https://images.unsplash.com/photo-1441986300917-64674bd600d8?w=1600&q=80')] bg-cover bg-no-repeat bg-top h-[60vh] w-full relative">
+            <div className='h-full w-full flex justify-center items-center'>
+                <div className='bg-black/50 absolute inset-0'></div>
+                <div className='text-center max-w-4xl space-y-3 z-10'>
+                    <h1 className='text-4xl font-extrabold text-gray-300  '><span className='italic text-amber-600'>Discover </span>Your Perfect  Shopping <br /><span className='italic text-amber-600'>Experience</span></h1>
+                    <p className='text-xl font-semibold text-gray-300'>Explore the latest collections, enjoy great discounts, and <br /> get your favorite products delivered fast.</p>
+                    <button className='bg-amber-900 px-4 py-2 rounded-2xl hover:bg-amber-700'>Shop Now</button>
+                </div>
+            </div>
 
-        </div >
+        </div>
+
     );
 };
 
